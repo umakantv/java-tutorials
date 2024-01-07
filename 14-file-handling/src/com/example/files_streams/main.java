9204fccdb8bbd52fbf1488637a8075806cf5f6f7
Umakant Vashishtha <41837826+umakantv@users.noreply.github.com>
2024-01-08 04:31:32 +0530

    File I/O

---
+++ 14-file-handling/src/com/example/files_streams/main.java
@@ -0,0 +1,100 @@
+package com.example.files_streams;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Random;
+
+class FileHandling {
+
+    final static String DictionaryFilePath = "/usr/share/dict/words";
+
+    public static void main(String[] args) {
+
+        System.out.println(new Date());
+
+        ArrayList<String> words = readFile(DictionaryFilePath);
+
+        writeWordsToFile(words, "output.txt");
+
+        System.out.println(new Date());
+    }
+
+    public static ArrayList<String> readFile(String filePath) {
+
+        ArrayList<String> words = new ArrayList<String>();
+
+        try {
+
+            BufferedReader br = new BufferedReader(
+                new InputStreamReader(
+                    new FileInputStream(filePath),
+                    "UTF-8"
+                )
+            );
+            String line = br.readLine();
+
+            while (line != null) {
+                words.add(line);
+                line = br.readLine();
+            }
+            br.close();
+
+            System.out.println("Total words : " + words.size());
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        return words;
+    }
+
+    public static void writeWordsToFile(ArrayList<String> words, String filePath) {
+        try {
+            File file = new File(filePath);
+            BufferedWriter bw = new BufferedWriter(
+                new OutputStreamWriter(
+                    new FileOutputStream(file, false),
+                    "UTF-8"
+                )
+            );
+
+            Random rand = new Random();
+
+            // 1 GB =  1073741824 bytes
+            // 100 MB = 104857600 bytes
+            final int charCount = 1073741824; // 1 GB
+            int size = 0, counterForGarbageCollection = 0;
+            String word = "";
+
+            // Get random words from the dictionary
+            while (size < charCount) {
+                word = words.get(rand.nextInt(words.size()));
+                size += word.length();
+
+                bw.write(word + " ");
+                // System.out.println(word);
+                // fileStream.write((word + " ").getBytes());
+
+                if (size > counterForGarbageCollection * (charCount / 10)) { // wait 10 times
+                    System.out.println("Waiting for Garbage Collection: " + counterForGarbageCollection);
+                    counterForGarbageCollection++;
+                    Thread.sleep(1000);
+                }
+            }
+
+            bw.close();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+}
