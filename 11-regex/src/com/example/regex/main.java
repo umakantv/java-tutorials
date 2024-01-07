108c95c757d4a303fa151140c19e4699e0e3069d
Umakant Vashishtha <41837826+umakantv@users.noreply.github.com>
2024-01-08 02:10:36 +0530

    Regex

---
+++ 11-regex/src/com/example/regex/main.java
@@ -0,0 +1,28 @@
+package com.example.regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class main {
+    public static void main(String[] args) {
+
+        // RegularExpression re = new RegularExpression();
+        // Pattern p = Pattern.compile("a{4}\\w{2}\\d{1}"); // aaaa\w\w\d
+        Pattern p = Pattern.compile("love"); // aaaa\w\w\d
+        String inputString = "Another love, another love";
+        Matcher matcher = p.matcher(inputString);
+
+        while (matcher.find()) {
+            System.out.println("Found match at: " + matcher.start() + " to " + matcher.end());
+            System.out.println(inputString.substring(matcher.start(), matcher.end()));
+        }
+
+        System.out.println("Email validation " + isValidEmail("balleballe@balle.balle"));
+    }
+
+    static boolean isValidEmail(String email) {
+        Pattern p = Pattern.compile("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$");
+        Matcher matcher = p.matcher(email);
+        return matcher.matches();
+    }
+}
